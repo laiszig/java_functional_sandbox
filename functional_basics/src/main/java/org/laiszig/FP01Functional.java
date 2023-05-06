392ce15be46d942dad062a82668b85be5eadac69
laiszig <laisziegler@gmail.com>
2023-05-06 15:47:43 -0300

    streams, method reference

--- functional_basics/src/main/java/org/laiszig/FP01Functional.java
+++ functional_basics/src/main/java/org/laiszig/FP01Functional.java
@@ -6,29 +6,34 @@ public class FP01Functional {
 
     public static void main(String[] args) {
 
-        printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+//        printAllNumbersInListStructure(numbers);
+        System.out.println("----------------");
+        printAllEvenNumbersInListFunctional(numbers);
     }
 
     private static void print(int number) {
         System.out.println(number);
     }
 
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-        //[12, 9, 13, 4, 6, 2, 4, 12, 15]
-        // 12
-        // 9
-        // 13
-        // 4
-        // ...
+//        numbers.stream()
+//                .forEach(FP01Functional::print); //Method Reference
 
         numbers.stream()
-                .forEach(FP01Functional::print);
+                .forEach(System.out::println);
+    }
 
-//        numbers.stream()
-//                .forEach(System.out::println);
-
-        for(int number:numbers) {
-            System.out.println(number);
-        }
+    private static void printAllEvenNumbersInListFunctional (List<Integer> numbers) {
+        //How to loop the numbers
+        numbers.stream()
+                //Filter - Only allow Even Numbers
+                .filter(FP01Functional::isEven)
+                .forEach(System.out::println);
     }
 }
--- functional_basics/src/main/java/org/laiszig/FP01Structural.java
+++ functional_basics/src/main/java/org/laiszig/FP01Structural.java
@@ -6,7 +6,11 @@ public class FP01Structural {
 
     public static void main(String[] args) {
 
-        printAllNumbersInListStructure(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
+//        printAllNumbersInListStructure(numbers);
+        System.out.println("----------------");
+        printAllEvenNumbersInListStructured(numbers);
     }
 
     private static void printAllNumbersInListStructure(List<Integer> numbers) {
@@ -14,6 +18,15 @@ public class FP01Structural {
         //How to loop the numbers
         for(int number:numbers) {
             System.out.println(number);
+        }
+    }
+
+    private static void printAllEvenNumbersInListStructured (List<Integer> evenNumbers) {
+        //How to loop the numbers
+        for(int number:evenNumbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
         }
     }
 }
