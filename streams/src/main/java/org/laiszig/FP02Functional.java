aa2aa98d8e680e38782d9b94b39bc78efcd234e8
laiszig <laisziegler@gmail.com>
2023-05-08 17:31:45 -0300

    java streams, distinct and sorted

--- streams/src/main/java/org/laiszig/FP02Functional.java
+++ streams/src/main/java/org/laiszig/FP02Functional.java
@@ -6,11 +6,19 @@ public class FP02Functional {
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Docker", "Kubernetes");
 
         int sum = addListFunctional(numbers);
         System.out.println(sum);
-
+        System.out.println("--------");
         numbers.stream().reduce(Integer.MIN_VALUE, (x,y) -> x>y ? x:y);
+
+        printDistinct(numbers);
+        System.out.println("--------");
+        sortNumbers(numbers);
+        System.out.println("--------");
+        sortCourses(courses);
+
     }
 
     private static int sum(int aggregate, int nextNumber) {
@@ -38,5 +46,23 @@ public class FP02Functional {
 //                .reduce(0, FP02Functional::sum);
 //                .reduce(0, (x,y) -> x+y);
                 .reduce(0, Integer::sum);
+    }
+
+    private static void printDistinct (List<Integer> numbers) {
+        numbers.stream()
+                .distinct()
+                .forEach(System.out::println);
+    }
+
+    private static void sortNumbers (List<Integer> numbers) {
+        numbers.stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static void sortCourses (List<String> courses) {
+        courses.stream()
+                .sorted()
+                .forEach(System.out::println);
     }
 }
