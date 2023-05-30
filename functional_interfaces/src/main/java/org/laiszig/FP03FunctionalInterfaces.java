7f9353e93d7e172ff72d7fd941f32523090ccfdf
laiszig <laisziegler@gmail.com>
2023-05-30 09:45:55 -0300

    functional interfaces methods

--- functional_interfaces/src/main/java/org/laiszig/FP03FunctionalInterfaces.java
+++ functional_interfaces/src/main/java/org/laiszig/FP03FunctionalInterfaces.java
@@ -19,42 +19,64 @@ public class FP03FunctionalInterfaces {
     }
 
     /**
-    In Java, a Consumer is an interface that represents a function that takes an input argument
-    of one type and returns no result. The Java Consumer interface is also a functional interface,
-    which means it can be used as the assignment target for a lambda expression or method reference.
-
-    The Consumer interface in Java has a single abstract method named "accept", which takes an input
-    argument of type T and returns no output. The "accept" method is used to perform some operation
-    on the input argument without returning any result.
-    */
+     * In Java, a Consumer is an interface that represents a function that takes an input argument
+     * of one type and returns no result. The Java Consumer interface is also a functional interface,
+     * which means it can be used as the assignment target for a lambda expression or method reference.
+     * <p>
+     * The Consumer interface in Java has a single abstract method named "accept", which takes an input
+     * argument of type T and returns no output. The "accept" method is used to perform some operation
+     * on the input argument without returning any result.
+     */
     private static Consumer<Integer> printConsumer() {
         return System.out::println;
     }
 
-    /**
-     In Java, a Function is an interface that represents a function that takes an input argument
-     of one type and returns an output of another type. The Java Function interface is also a
-     functional interface, which means it can be used as the assignment target for a lambda expression
-     or method reference.
+    Consumer<Integer> printConsumer2 = new Consumer<Integer>() {
+        @Override
+        public void accept(Integer x) {
+            System.out.println(x);
+        }
+    };
 
-     The Function interface in Java has a single abstract method named "apply", which takes an input
-     argument of type T and returns an output of type R. The "apply" method is used to transform the
-     input argument into an output of a different type.
+
+    /**
+     * In Java, a Function is an interface that represents a function that takes an input argument
+     * of one type and returns an output of another type. The Java Function interface is also a
+     * functional interface, which means it can be used as the assignment target for a lambda expression
+     * or method reference.
+     * <p>
+     * The Function interface in Java has a single abstract method named "apply", which takes an input
+     * argument of type T and returns an output of type R. The "apply" method is used to transform the
+     * input argument into an output of a different type.
      **/
     private static Function<Integer, Integer> squareFunction() {
         return x -> x * x;
     }
 
-    /**
-    In Java, a predicate is an interface that represents a function that takes
-    a single input and returns a Boolean value. The Java Predicate interface is a functional interface,
-    which means it can be used as the assignment target for a lambda expression or method reference.
+    Function<Integer, Integer> squareFunction2 = new Function<Integer, Integer>() {
+        @Override
+        public Integer apply(Integer x) {
+            return x * x;
+        }
+    };
 
-    The Predicate interface in Java has a single abstract method named "test", which takes an input
-    argument and returns a Boolean value. The "test" method is used to evaluate the input argument
-    against some condition and returns true if the condition is satisfied, otherwise it returns false.
-    **/
+    /**
+     * In Java, a predicate is an interface that represents a function that takes
+     * a single input and returns a Boolean value. The Java Predicate interface is a functional interface,
+     * which means it can be used as the assignment target for a lambda expression or method reference.
+     * <p>
+     * The Predicate interface in Java has a single abstract method named "test", which takes an input
+     * argument and returns a Boolean value. The "test" method is used to evaluate the input argument
+     * against some condition and returns true if the condition is satisfied, otherwise it returns false.
+     **/
     private static Predicate<Integer> isEvenPredicate() {
         return x -> x % 2 == 0;
     }
+
+    Predicate<Integer> isEvenPredicate2 = new Predicate<Integer>() {
+        @Override
+        public boolean test(Integer x) {
+            return x % 2 == 0;
+        }
+    };
 }
